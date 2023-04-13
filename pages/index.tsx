1d857f40555493290957b351e832b33708a3fca2
Matheus Eduardo <40497869+Matheus-Azevedo@users.noreply.github.com>
2023-04-13 22:25:33 +0000

    chore: update gitignore and index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 function Home(): JSX.Element {
   return (
-    <div> Projeto AZNEWS </div>
+    <section>
+      <h1>Projeto AZNEWS</h1>
+      <p>Este projeto é a implementação do https://www.tabnews.com.br/ para https://curso.dev/ do Felipe Deschamps</p>
+    </section>
   )
 }
 
