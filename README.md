47507598e701a11ae678311441cd1437cc3caa76
Matheus Eduardo <40497869+Matheus-Azevedo@users.noreply.github.com>
2023-06-29 03:48:42 +0000

    add scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # aznews.com.br
+
 Este projeto é a implementação do https://www.tabnews.com.br/ para https://curso.dev/ do Felipe Deschamps
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,17 @@ function Home(): JSX.Element {
   return (
     <section>
       <h1>Projeto AZNEWS</h1>
-      <p>Este projeto é a implementação do https://www.tabnews.com.br/ para https://curso.dev/ do Felipe Deschamps</p>
+      <p>
+        Este projeto é a implementação do https://www.tabnews.com.br/ para
+        https://curso.dev/ do Felipe Deschamps
+      </p>
       <p>Este projeto é um projeto de estudo, não tem fins lucrativos.</p>
-      <p>Lembre-se: Depois que você decidir o que quer fazer da vida, não arrede os pés nem para pegar impulso!</p>
+      <p>
+        Lembre-se: Depois que você decidir o que quer fazer da vida, não arrede
+        os pés nem para pegar impulso!
+      </p>
     </section>
-  )
+  );
 }
 
 export default Home;
