19506b426b4c030dcaa2a5a2aba72f5e7a8f59bf
Matheus Eduardo <40497869+Matheus-Azevedo@users.noreply.github.com>
2023-08-29 18:32:00 +0000

    Testando conceito: Teste com Jest

---
+++ Concept/calculadora.js
@@ -0,0 +1,20 @@
+function somar(number1, number2) {
+  return number1 + number2;
+}
+
+function subtrair(number1, number2) {
+  return number1 - number2;
+}
+
+function multiplicar(number1, number2) {
+  return number1 * number2;
+}
+
+function dividir(number1, number2) {
+  return number1 / number2;
+}
+
+exports.somar = somar;
+exports.subtrair = subtrair;
+exports.multiplicar = multiplicar;
+exports.dividir = dividir;
