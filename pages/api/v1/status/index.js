c2554b982f7996716a1d306ee0a2e508d76bb853
Matheus Eduardo <40497869+Matheus-Azevedo@users.noreply.github.com>
2023-10-20 12:14:13 +0000

    Criando o modulo database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(_request, response) {
+import database from "../../../../infra/database";
+
+async function status(_request, response) {
+  const result = await database.query("SELECT 1 + 1 AS solution;");
+  console.log(result.rows[0]);
   response.status(200).json({
     status: "OK",
     message: "API is running - CURSO.DEV is awesome!",
