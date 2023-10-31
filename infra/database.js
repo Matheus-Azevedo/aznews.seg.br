6cfee2b9c76a37a827f78fea5870ca185318158c
Matheus Eduardo <40497869+Matheus-Azevedo@users.noreply.github.com>
2023-10-31 02:59:41 +0000

    add: databse and compose files

--- infra/database.js
+++ infra/database.js
@@ -2,10 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "local_password",
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
   const result = await client.query(queryObject);
