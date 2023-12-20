1f0924fa0fcd8f928ab4a62f9e4802b5474d474b
Matheus Eduardo <40497869+Matheus-Azevedo@users.noreply.github.com>
2023-12-20 10:40:20 +0000

    Finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,35 @@
 import database from "infra/database";
 
-async function status(_request, response) {
-  const result = await database.query("SELECT 1 + 1 AS solution;");
-  console.log(result.rows[0]);
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const queryDatabaseVersionResult = await database.query({
+    text: "SHOW server_version;",
+  });
+
+  const queryMaxConnectionsResult = await database.query({
+    text: "SHOW max_connections;",
+  });
+
+  const databaseName = process.env.POSTGRES_DB;
+  const queryOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseVersion = queryDatabaseVersionResult.rows[0];
+  const maxConnections = queryMaxConnectionsResult.rows[0].max_connections;
+  const openedConnections = queryOpenedConnectionsResult.rows[0].count;
+
   response.status(200).json({
-    status: "OK",
-    message: "API is running - CURSO.DEV is awesome!",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.server_version,
+        max_connections: parseInt(maxConnections, 10),
+        openedConnections: openedConnections,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,16 +1,33 @@
 const url = "http://localhost:3000/api/v1/status";
 
 describe("GET /api/v1/status", () => {
-  test('should return 200 and { status: "OK" }', async () => {
+  test("should return 200", async () => {
     const response = await fetch(url);
     expect(response.status).toBe(200);
   });
-  test('should return a body { status: "OK", message: "API is running - CURSO.DEV is awesome!" }', async () => {
+
+  test("Should return a JSON with updated_at", async () => {
     const response = await fetch(url);
-    const body = await response.json();
-    expect(body).toEqual({
-      status: "OK",
-      message: "API is running - CURSO.DEV is awesome!",
-    });
+    const responseBody = await response.json();
+    const updatedAt = new Date(responseBody.updated_at).toISOString();
+    expect(responseBody.updated_at).toBe(updatedAt);
+  });
+
+  test("Should return a JSON with database information - Version", async () => {
+    const response = await fetch(url);
+    const responseBody = await response.json();
+    expect(responseBody.dependencies.database.version).toEqual("16.0");
+  });
+
+  test("Should return a JSON with database information - Max Connections", async () => {
+    const response = await fetch(url);
+    const responseBody = await response.json();
+    expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  });
+
+  test("Should return a JSON with database information - Used Connections", async () => {
+    const response = await fetch(url);
+    const responseBody = await response.json();
+    expect(responseBody.dependencies.database.openedConnections).toEqual(1);
   });
 });
